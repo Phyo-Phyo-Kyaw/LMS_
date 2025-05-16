4e0b9f2098527b7e0d5977183680e18ef1567f5f
Phyo Phyo Kyaw <51367232+Phyo-Phyo-Kyaw@users.noreply.github.com>
2025-05-16 09:10:44 +0630

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,22 +1,15 @@
-const mysql = require('mysql2/promise');
-const dotenv = require('dotenv');
-
+// db.js
+import pkg from 'pg';
+import dotenv from 'dotenv';
 dotenv.config();
 
-async function initializeDatabase() {
-  try {
-    const connection = await mysql.createConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME
-    });
-    console.log('Connected to MySQL database.');
-    return connection;
-  } catch (err) {
-    console.error('Error connecting to MySQL:', err);
-    process.exit(1); // stop app if no db connection
-  }
-}
+const { Pool } = pkg;
 
-module.exports = initializeDatabase();
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // required for Render's PostgreSQL
+  },
+});
+
+export default db;
