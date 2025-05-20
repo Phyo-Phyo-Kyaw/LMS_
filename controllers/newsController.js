ddb426ea76add023118ed8e039d17188e4699c68
Phyo Phyo Kyaw <51367232+Phyo-Phyo-Kyaw@users.noreply.github.com>
2025-05-20 14:36:26 +0630

    Update newsController.js

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -1,12 +1,11 @@
-const dbPromise = require('../config/db');
+const pool = require('../config/db');
 
 exports.getAllNews = async (req, res) => {
   try {
-    const db = await dbPromise;
-    const [news] = await db.query('SELECT * FROM news');
+    const result = await pool.query('SELECT * FROM news');
+    const news = result.rows;
     res.render('news/index', { news }); 
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
-
